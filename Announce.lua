71680e556c30b06541eed96494eea7cfa8ebb59f
slaren <slaren@7293d75e-4ea7-4fe5-a83b-d422c3ac61a1>
2011-01-19 15:38:47 +0000

    - Added compact reporting mode - Added support for reporting to custom channels - Added health to reports - Changed display filters defaults to include everything - Removed "my pet" from the default capture filter - Removed "others time" data capture option for now, since the feature won't be available for a while anyway - As usual, more random minor fixes and improvements

--- Announce.lua
+++ Announce.lua
@@ -6,30 +6,30 @@ function DeathNote:AnnounceDeath(death)
 	if not self.settings.announce.enable then
 		return
 	end
-	
+
 	local now = GetTime()
 	local tensecs = now - 10
-	
+
 	for i = #announced_deaths, 1, -1 do
 		if announced_deaths[i] < tensecs then
 			table.remove(announced_deaths, i)
 		end
 	end
-	
+
 	if #announced_deaths >= self.settings.announce.limit then
 		skipped_deaths = skipped_deaths + 1
 		self:CancelTimer(skip_timer, true)
 		skip_timer = self:ScheduleTimer("SkipAnnounce", 10)
 		return
 	end
-	
+
 	local entry, damage, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing =  DeathNote:GetKillingBlow(death)
-	
+
 	local isoutputchat = self:O_IsChatOutput(self.settings.announce.channel)
 	local iswhisper = self.settings.announce.channel == "WHISPER"
-	
+
 	local text
-	
+
 	if not entry then
 		if self.settings.announce.announce_unknown then
 			text = string.format("%s|r has died of a heart attack", self:FormatUnit(death[2], death[3], death[4]))
@@ -41,24 +41,24 @@ function DeathNote:AnnounceDeath(death)
 			text = self:FormatCombatLog(entry)
 		elseif self.settings.announce.style == "FORMATTED" then
 			local source = self:FormatUnit(entry[5], entry[6], entry[7])
-			local spell = self:FormatAnnounceSpell(entry)
-			
+			local spell = self:FormatEntrySpell(entry)
+
 			if iswhisper then
 				text = "You were killed by "
 			else
 				text = string.format("%s|r was killed by ", self:FormatUnit(death[2], death[3], death[4]))
 			end
-			
+
 			if source ~= "" then
 				text = text .. string.format("%s's|r %s", source, spell)
 			else
 				text = text .. spell
 			end
-			
+
 			if self.settings.announce.format_damage and damage > 0 then
 				text = text .. string.format(" (%i %s", damage, DAMAGE)
 			end
-			
+
 			if self.settings.announce.format_hittype then
 				if critical then
 					text = text .. " " .. TEXT_MODE_A_STRING_RESULT_CRITICAL
@@ -74,7 +74,7 @@ function DeathNote:AnnounceDeath(death)
 			if self.settings.announce.format_damage and damage > 0 then
 				text = text .. ") "
 			end
-			
+
 			if self.settings.announce.format_resist then
 				if resisted and resisted > 0 then
 					text = text .. string.format(TEXT_MODE_A_STRING_RESULT_RESIST, resisted) .. " "
@@ -86,29 +86,30 @@ function DeathNote:AnnounceDeath(death)
 					text = text .. string.format(TEXT_MODE_A_STRING_RESULT_ABSORB, absorbed) .. " "
 				end
 			end
-			
+
 			if self.settings.announce.format_overkill then
 				if overkill and overkill > 0 then
 					text = text .. string.format(TEXT_MODE_A_STRING_RESULT_OVERKILLING, overkill)
 				end
 			end
-			
+
 			text = string.gsub(text, "^%s*(.-)%s*$", "%1") -- trim spaces
 		end
 	end
-	
+
 	if isoutputchat then
 		text = self:CleanForChat(text)
 	else
+		-- add the icon borders back again
 		text = text:gsub("|T(.-):.-|t", "|T%1:0|t")
 	end
-	
+
 	if iswhisper then
 		text = { death[3], text }
 	end
-	
+
 	self:O_Send(self.settings.announce.channel, text)
-	
+
 	table.insert(announced_deaths, now)
 end
 
--- DeathNote.lua
+++ DeathNote.lua
@@ -11,7 +11,7 @@ function DeathNote:OnInitialize()
 			max_deaths = 50,
 			death_time = 30,
 			others_death_time = 0,
-			
+
 			unit_filters = {
 				group = true,
 				my_pet = true,
@@ -21,7 +21,7 @@ function DeathNote:OnInitialize()
 				friendly_npcs = false,
 				enemy_npcs = false,
 			},
-			
+
 			display = {
 				x = 0, y = 0, w = 700, h = 500,
 				namelist_width = 220,
@@ -31,27 +31,27 @@ function DeathNote:OnInitialize()
 				scale = 1,
 				columns = { 60, 90, 60, 100 },
 			},
-			
+
 			display_filters = {
 				damage_threshold = 0,
 				hide_misses = false,
-				consolidate_damage = true,
-				
+				consolidate_damage = false,
+
 				heal_threshold = 0,
-				consolidate_heals = true,
-				
-				buff_gains = false,
-				buff_fades = false,
-				debuff_gains = false,
-				debuff_fades = false,
+				consolidate_heals = false,
+
+				buff_gains = true,
+				buff_fades = true,
+				debuff_gains = true,
+				debuff_fades = true,
 				survival_buffs = true,
 				highlight_survival = true,
 				consolidate_auras = true,
-				
+
 				spell_filter = {},
 				source_filter = {},
 			},
-			
+
 			announce = {
 				enable = false,
 				announce_unknown = false,
@@ -63,15 +63,23 @@ function DeathNote:OnInitialize()
 				format_overkill = true,
 				format_hittype = true,
 			},
+			
+			report = {
+				max_lines = 15,
+				style = "COMBAT_LOG",
+			},
 		},
 	})
-	
+
 	self.settings = self.db.profile
 	
+	-- Clean options -- TODO: remove this when implemented
+	self.settings.others_death_time = 0
+
 	-- Register options
 	LibStub("AceConfig-3.0"):RegisterOptionsTable("Death Note", self.Options)
 	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Death Note", "Death Note")
-	
+
 	-- Register LDB object
 	self.ldb = LibStub("LibDataBroker-1.1"):NewDataObject("DeathNote", {
 		type = "data source",
@@ -99,7 +107,7 @@ function DeathNote:OnInitialize()
 			tooltip:AddLine("|cffeda55fClick|r to open DeathNote. |cffeda55fRight-Click|r to show options. |cffeda55fShift-Click|r to optimize data. |cffeda55fCtrl-Click|r to reset data.", 0.2, 1, 0.2, 1)
 		end,
 	})
-	
+
 	self:DataCapture_Initialize()
 end
 
@@ -109,11 +117,11 @@ function DeathNote:OnEnable()
 	self:RegisterEvent("PLAYER_REGEN_DISABLED")
 	self:RegisterEvent("PLAYER_FLAGS_CHANGED")
 	self:RegisterEvent("PLAYER_LEAVING_WORLD")
-	
+
 	self:AddToUnitPopup()
-	
+
 	self:ScheduleRepeatingTimer("UpdateLDB", 5)
-	
+
 	if self.settings.debugging then
 		self:Show()
 	end
@@ -135,30 +143,45 @@ local lt = GetTime()
 function DeathNote:UpdateLDB()
 	UpdateAddOnMemoryUsage()
 	self.ldb.text = string.format("%i deaths - %i KB", #DeathNoteData.deaths, floor(GetAddOnMemoryUsage("DeathNote") + 0.5))
-	
+
 	lt = GetTime()
 end
 
-function DeathNote:SendReport(channel)
-	local max_lines = 10
+------------------------------------------------------------------------------
+-- Reports
+------------------------------------------------------------------------------
 
-	local target	
+function DeathNote:SendReport(channel, arg)
+	local target
 	if channel == "WHISPER" then
 		target = UnitName("target")
-	end
-	
-	if self.dropdown_line > max_lines then
-		self:Print(string.format("Limiting report to %i lines", max_lines))
+		if not target then
+			return
+		end
+	elseif channel == "CHANNEL" then
+		target = arg
 	end
 
-	local msg  = string.format("DeathNote: Death report for %s at %s", self.current_death[3], date("%X", self.current_death[1]))
+	local func
+	if self.settings.report.style == "COMBAT_LOG" then
+		func = self.FormatReportCombatLog
+	else
+		func = self.FormatReportCompact
+	end
+
+	local msg = string.format("DeathNote: Death report for %s at %s", self.current_death[3], date("%X", self.current_death[1]))
 	SendChatMessage(msg, channel, nil, target)
-	
-	for i = self.dropdown_line, math.max(1, self.dropdown_line - max_lines + 1), -1 do
+
+	self.report_line_count = 0
+	for i = self.dropdown_line, 1, -1 do
+		if self.report_line_count >= self.settings.report.max_lines then
+			self:Print(string.format("Limiting report to %i lines", self.settings.report.max_lines))
+			break
+		end
+
 		local entry = self.logframe:GetLineUserdata(i)
-		local timestamp = entry[3]
-	
-		local msg = string.format("[%.01f s] %s", timestamp - self.current_death[1], self:FormatChatAmount(entry))
-		SendChatMessage(msg, channel, nil, target)
-	end	
+
+		func(self, entry, channel, target)
+	end
+	self.report_line_count = nil
 end
