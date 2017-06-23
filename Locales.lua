9c4b1afb6fe7e9f947a0d95ca90b02b9dd705f7a
axiomasham <axio@axio.name>
2017-06-23 20:58:34 +0000

    CHANGELOG.html: 70200.1-release Locales.lua: localizations are updated UI.lua: fixed bug with incorrect frame strata of filter button

--- Locales.lua
+++ Locales.lua
@@ -1,4 +1,4 @@
-﻿local L
+local L
 
 local debug = false
 --@debug@
@@ -1495,72 +1495,51 @@ L["<Refresh>"] = "<Refresh>"
 L["<Steal>"] = "<Steal>"
 --Translation missing 
 L["All friendly players, including those not in your group"] = "All friendly players, including those not in your group"
---Translation missing 
-L["Amount"] = "Amount"
---Translation missing 
-L["Announce"] = "Announce"
---Translation missing 
-L["Announce deaths"] = "Announce deaths"
---Translation missing 
-L["Announce deaths with an unknown cause"] = "Announce deaths with an unknown cause"
---Translation missing 
-L["Announces/10 seconds limit"] = "Announces/10 seconds limit"
---Translation missing 
-L["Auras"] = "Auras"
---Translation missing 
-L["Bar"] = "Bar"
+L["Amount"] = "Количество"
+L["Announce"] = "Объявления"
+L["Announce deaths"] = "Объявлять смерти"
+L["Announce deaths with an unknown cause"] = "Объявлять смерти по неизвестной причине"
+L["Announces/10 seconds limit"] = "Лимит объявлений за период в 10 секунд"
+L["Auras"] = "Ауры"
+L["Bar"] = "Полоски"
 --Translation missing 
 L["Battleground"] = "Battleground"
---Translation missing 
-L["Buff fades"] = "Buff fades"
---Translation missing 
-L["Buff gains"] = "Buff gains"
+L["Buff fades"] = "Окончание баффа"
+L["Buff gains"] = "Получение баффа"
 --Translation missing 
 L["Channel"] = "Channel"
 --Translation missing 
 L["Chat frame"] = "Chat frame"
 --Translation missing 
 L["Check the units you are interested in. Data for the units not filtered is discarded."] = "Check the units you are interested in. Data for the units not filtered is discarded."
---Translation missing 
-L["Combat log lines"] = "Combat log lines"
+L["Combat log lines"] = "Как в логе боя"
 --Translation missing 
 L["Compact"] = "Compact"
---Translation missing 
-L["Consolidate consecutive auras"] = "Consolidate consecutive auras"
---Translation missing 
-L["Consolidate consecutive heals"] = "Consolidate consecutive heals"
---Translation missing 
-L["Consolidate consecutive hits"] = "Consolidate consecutive hits"
---Translation missing 
-L["Damage"] = "Damage"
---Translation missing 
-L["Data capture"] = "Data capture"
+L["Consolidate consecutive auras"] = "Объединить последовательные ауры"
+L["Consolidate consecutive heals"] = "Объединить последовательное исцеление"
+L["Consolidate consecutive hits"] = "Объединить последовательный урон"
+L["Damage"] = "Урон"
+L["Data capture"] = "Запись данных"
 --Translation missing 
 L["Data has been reset"] = "Data has been reset"
 --Translation missing 
 L["Data optimization done in %.02f ms"] = "Data optimization done in %.02f ms"
 --Translation missing 
 L["Death"] = "Death"
---Translation missing 
 L["Death Note"] = "Death Note"
 --Translation missing 
 L["Death Note: Death report for %s at %s"] = "Death Note: Death report for %s at %s"
---Translation missing 
-L["Debuff fades"] = "Debuff fades"
---Translation missing 
-L["Debuff gains"] = "Debuff gains"
---Translation missing 
-L["Decrease scale"] = "Decrease scale"
+L["Debuff fades"] = "Окончание дебаффа"
+L["Debuff gains"] = "Получение дебаффа"
+L["Decrease scale"] = "Уменьшить масштаб"
 --Translation missing 
 L[ [=[Enable this if you want the data to persist after logging out or after a reload ui.
 Keep in mind that depending on your options this may generate a very big SavedVariables file and may impact your login/logout and reload ui times.]=] ] = [=[Enable this if you want the data to persist after logging out or after a reload ui.
 Keep in mind that depending on your options this may generate a very big SavedVariables file and may impact your login/logout and reload ui times.]=]
 --Translation missing 
 L["Enabling this option will taint the unit popup menu and will prevent some options from working (such as setting a focus target)"] = "Enabling this option will taint the unit popup menu and will prevent some options from working (such as setting a focus target)"
---Translation missing 
-L["Enemy NPCs"] = "Enemy NPCs"
---Translation missing 
-L["Enemy players"] = "Enemy players"
+L["Enemy NPCs"] = "Враждебные NPC"
+L["Enemy players"] = "Враждебные игроки"
 --Translation missing 
 L[ [=[Enter one or more sources, separated by commas.
 Ctrl+Click on a source column to add that source.]=] ] = [=[Enter one or more sources, separated by commas.
@@ -1569,70 +1548,44 @@ Ctrl+Click on a source column to add that source.]=]
 L[ [=[Enter one or more spells, separated by commas.
 Ctrl+Click on a spell column to add that spell.]=] ] = [=[Enter one or more spells, separated by commas.
 Ctrl+Click on a spell column to add that spell.]=]
---Translation missing 
-L["Filters"] = "Filters"
---Translation missing 
-L["Formatted"] = "Formatted"
---Translation missing 
-L["Formatted style options"] = "Formatted style options"
---Translation missing 
-L["Friendly NPCs"] = "Friendly NPCs"
---Translation missing 
-L["Friendly players"] = "Friendly players"
---Translation missing 
-L["General"] = "General"
+L["Filters"] = "Фильтры"
+L["Formatted"] = "Отформатированные"
+L["Formatted style options"] = "Настройки форматирования"
+L["Friendly NPCs"] = "NPC-союзники"
+L["Friendly players"] = "Игроки-союзники"
+L["General"] = "Общее"
 --Translation missing 
 L["Group (party or raid)"] = "Group (party or raid)"
---Translation missing 
-L["Group players"] = "Group players"
+L["Group players"] = "Игроки в группе (рейде)"
 --Translation missing 
 L["Guild"] = "Guild"
---Translation missing 
-L["Healing"] = "Healing"
---Translation missing 
-L["Health format"] = "Health format"
---Translation missing 
-L["Hide misses"] = "Hide misses"
---Translation missing 
-L["Highlight survival cooldowns"] = "Highlight survival cooldowns"
---Translation missing 
-L["HP"] = "HP"
---Translation missing 
-L["HP %"] = "HP %"
---Translation missing 
-L["HP/HPMax"] = "HP/HPMax"
---Translation missing 
-L["Include amount resisted/blocked/absorbed"] = "Include amount resisted/blocked/absorbed"
---Translation missing 
-L["Include damage"] = "Include damage"
---Translation missing 
-L["Include hit type (critical, crushing, etc)"] = "Include hit type (critical, crushing, etc)"
---Translation missing 
-L["Include overkill"] = "Include overkill"
---Translation missing 
-L["Increase scale"] = "Increase scale"
---Translation missing 
-L["Keep data between sessions"] = "Keep data between sessions"
+L["Healing"] = "Исцеление"
+L["Health format"] = "Вид ХП"
+L["Hide misses"] = "Скрыть промахи"
+L["Highlight survival cooldowns"] = "Подсвечивать защитные способности"
+L["HP"] = "Количество ХП"
+L["HP %"] = "Процент ХП"
+L["HP/HPMax"] = "ХП / макс. ХП"
+L["Include amount resisted/blocked/absorbed"] = "Включать количество отраженного/заблокированного/поглощенного"
+L["Include damage"] = "Включать урон"
+L["Include hit type (critical, crushing, etc)"] = "Включать тип урона (критический, сокрушающий и т.п.)"
+L["Include overkill"] = "Включать избыточный урон (overkill)"
+L["Increase scale"] = "Увеличить масштаб"
+L["Keep data between sessions"] = "Сохранять данные между сессиями"
 --Translation missing 
 L["Limiting report to %i lines"] = "Limiting report to %i lines"
---Translation missing 
-L["Maximum number of deaths"] = "Maximum number of deaths"
+L["Maximum number of deaths"] = "Макс. количество смертей"
 --Translation missing 
 L["Miss"] = "Miss"
---Translation missing 
-L["Name"] = "Name"
+L["Name"] = "Имя"
 --Translation missing 
 L["None"] = "None"
 --Translation missing 
 L["Officer"] = "Officer"
---Translation missing 
-L["Options"] = "Options"
---Translation missing 
-L["Other pets"] = "Other pets"
---Translation missing 
-L["Others"] = "Others"
---Translation missing 
-L["Output channel"] = "Output channel"
+L["Options"] = "Настройки"
+L["Other pets"] = "Другие питомцы"
+L["Others"] = "Другое"
+L["Output channel"] = "Канал вывода"
 --Translation missing 
 L["Party"] = "Party"
 --Translation missing 
@@ -1641,58 +1594,41 @@ L["Party and raid members, including yourself"] = "Party and raid members, inclu
 L["Raid"] = "Raid"
 --Translation missing 
 L["Raid Warning"] = "Raid Warning"
---Translation missing 
-L["Real time"] = "Real time"
+L["Real time"] = "Реальное время"
 --Translation missing 
 L["Report style"] = "Report style"
---Translation missing 
-L["Reset"] = "Reset"
---Translation missing 
-L["Reset data"] = "Reset data"
+L["Reset"] = "Сбросить"
+L["Reset data"] = "Сбросить данные"
 --Translation missing 
 L["Say"] = "Say"
---Translation missing 
-L["Scale: %i%%"] = "Scale: %i%%"
---Translation missing 
-L["Seconds from death"] = "Seconds from death"
+L["Scale: %i%%"] = "Масштаб: %i%%"
+L["Seconds from death"] = "Секунды до смерти"
 --Translation missing 
 L["Seconds to keep before each death (for other units)"] = "Seconds to keep before each death (for other units)"
---Translation missing 
-L["Seconds to keep before each death (for the unit killed)"] = "Seconds to keep before each death (for the unit killed)"
+L["Seconds to keep before each death (for the unit killed)"] = "Сколько секунд записывать перед каждой смертью (для убитого существа)"
 --Translation missing 
 L["Send report from this line"] = "Send report from this line"
 --Translation missing 
 L["Setting scale to %i%%"] = "Setting scale to %i%%"
 --Translation missing 
 L["Show Death Note"] = "Show Death Note"
---Translation missing 
-L["Show in the unit popup menu (requires a UI reload)"] = "Show in the unit popup menu (requires a UI reload)"
+L["Show in the unit popup menu (requires a UI reload)"] = "Показывать в выпадающем меню игроков (требуется перезагрузка UI)"
 --Translation missing 
 L["Show target deaths"] = "Show target deaths"
---Translation missing 
-L["Sort deaths by"] = "Sort deaths by"
---Translation missing 
-L["Source"] = "Source"
---Translation missing 
-L["Source filter"] = "Source filter"
---Translation missing 
-L["Spell"] = "Spell"
---Translation missing 
-L["Spell filter"] = "Spell filter"
---Translation missing 
-L["Survival cooldowns"] = "Survival cooldowns"
+L["Sort deaths by"] = "Сортировать смерти по..."
+L["Source"] = "Источник"
+L["Source filter"] = "Фильтр источников"
+L["Spell"] = "Заклинание"
+L["Spell filter"] = "Фильтр заклинаний"
+L["Survival cooldowns"] = "Защитные способности"
 --Translation missing 
 L["The effect of this filter depends on the other filters. For example, if you have the friendly players filter inactive, their pets deaths won't be recorded either, even with this filter activated."] = "The effect of this filter depends on the other filters. For example, if you have the friendly players filter inactive, their pets deaths won't be recorded either, even with this filter activated."
 --Translation missing 
 L["This data is used to display actions of other players when a death happened. Set this value to 0 unless you want to use this feature, as it can use a large amount of memory."] = "This data is used to display actions of other players when a death happened. Set this value to 0 unless you want to use this feature, as it can use a large amount of memory."
---Translation missing 
-L["Threshold"] = "Threshold"
---Translation missing 
-L["Time"] = "Time"
---Translation missing 
-L["Time format"] = "Time format"
---Translation missing 
-L["Units filters"] = "Units filters"
+L["Threshold"] = "Порог"
+L["Time"] = "Время"
+L["Time format"] = "Формат времени"
+L["Units filters"] = "Фильтр существ"
 --Translation missing 
 L["Unknown"] = "Unknown"
 --Translation missing 
@@ -1701,8 +1637,7 @@ L["Whisper"] = "Whisper"
 L["Whisper target"] = "Whisper target"
 --Translation missing 
 L["You were killed by"] = "You were killed by"
---Translation missing 
-L["Your pet"] = "Your pet"
+L["Your pet"] = "Ваш питомец"
 
 end
 
