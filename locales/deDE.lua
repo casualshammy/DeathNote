fcaf7b602d6698c8bfade9f5397730f89fe9e6b4
Alexander Sommen <axio@axio.name>
2020-08-30 13:17:49 +0100

    DeathNote.toc: Locales.lua: locales/*:   - added auto-packaging of locales pkgmeta.yaml:   - deleted AceGUI-3.0 packaging, because it doesn't support Shadowlands     Temporarily using custom version of AceGUI-3.0 UI.lua:   - initial Shadowlands support libs/AceGUI-3.0/*:   - custom version of AceGUI-3.0 for Shadowlands

---
+++ locales/deDE.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("DeathNote", "deDE");
+L = L or {}
+--[===[@non-debug@
+@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", table-name="L")@
+--@end-non-debug@]===]
---
+++ locales/ptBR.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("DeathNote", "ptBR");
+L = L or {}
+--[===[@non-debug@
+@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="english", table-name="L")@
+--@end-non-debug@]===]
